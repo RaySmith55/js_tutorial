dbf4d5ac74da01b023c68686421b0ff55aefa162
Ray Smith <ray.smith@sky.uk>
2018-06-07 17:45:29 +0100

    Complete chapter 7.1, improve palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,20 @@
 // Reverses a string
-
 function reverse(string) {
     return Array.from(string).reverse().join("");
 }
 
-function palindrome(string) {
-    let processedContent = string.toLowerCase();
-    return processedContent === reverse(processedContent);
+// Defines a Phrase object
+function Phrase(content) {
+    this.content = content;
+
+    // Makes the phrase LOUDER
+    this.louder = function() {
+        return this.content.toUpperCase();
+    }
+
+    // Returns true for a palindrome, false otherwise
+    this.palindrome = function palindrome() {
+        let processedContent = this.content.toLowerCase();
+        return processedContent === reverse(processedContent);
+    }
 }
