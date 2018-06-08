602eeb114e2212b852cd7055106a7f37c9d289de
Ray Smith <ray.smith@sky.uk>
2018-06-08 17:24:35 +0100

    Add TranslatedPhrase object and processor method

--- palindrome.js
+++ palindrome.js
@@ -7,6 +7,15 @@ function reverse(string) {
 function Phrase(content) {
     this.content = content;
 
+    this.processor = function(string) {
+        return string.toLowerCase();
+    }
+
+    // Returns content processed for palindrome testing
+    this.processedContent = function processedContent() {
+        return this.processor(this.content);
+    }
+
     // Makes the phrase LOUDER
     this.louder = function() {
         return this.content.toUpperCase();
@@ -14,7 +23,19 @@ function Phrase(content) {
 
     // Returns true for a palindrome, false otherwise
     this.palindrome = function palindrome() {
-        let processedContent = this.content.toLowerCase();
-        return processedContent === reverse(processedContent);
+        return this.processedContent() === reverse(this.processedContent());
     }
 }
+
+// Defines a translated Phrase object
+function TranslatedPhrase(content, translation) {
+    this.content = content;
+    this.translation = translation;
+
+    // Returns translation processed for palindrome testing
+    this.processedContent = function processedContent() {
+        return this.processor(this.translation);
+    }
+}
+
+TranslatedPhrase.prototype = new Phrase();
